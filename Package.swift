29782048abd4f19d4714e535aae2489ca8b5a010
Adrian Herridge <editfmah@yahoo.co.uk>
2019-08-28 13:24:35 +0100

    .

--- Package.swift
+++ Package.swift
@@ -22,8 +22,5 @@ let package = Package(
         .target(
             name: "Switchblade",
             dependencies: ["Kassandra","CryptoSwift"]),
-        .testTarget(
-            name: "SwitchbladeTests",
-            dependencies: ["Switchblade"]),
     ]
 )
