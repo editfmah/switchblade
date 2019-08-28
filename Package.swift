a51ef7b8873a2c1a6899d4003bfe6b068c6fe440
Adrian Herridge <editfmah@yahoo.co.uk>
2019-08-28 13:31:43 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Switchblade",
-            dependencies: ["Kassandra","CryptoSwift"]),
-            ssource: "./Sources/switchblade",
+            dependencies: ["Kassandra","CryptoSwift"],
+            path: "./Sources/switchblade"),
     ]
 )
