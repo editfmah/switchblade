8184add5029729b0f388543a9215e156b1b6bbbd
Adrian Herridge <editfmah@yahoo.co.uk>
2020-12-02 13:59:02 +0000

    missed file for check in

--- Sources/switchblade/Core/protocols/SwitchbladeBindable.swift
+++ Sources/switchblade/Core/protocols/SwitchbladeBindable.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+public protocol SwitchbadeBinder {
+    
+    @discardableResult func bind<T: Codable>(key: KeyType,_ onChange: ((T?)->Void)?) -> Binding<T>
+    @discardableResult func bind<T: Codable>(_ object: T,_ onChange: ((T?)->Void)?) -> Binding<T>
+    @discardableResult func bind<T: Codable>(key: KeyType, keyspace: String,_ onChange: ((T?)->Void)?) -> Binding<T>
+    @discardableResult func bind<T: Codable>(_ onChange: (([T]?)->Void)?) -> BindingCollection<T>
+    @discardableResult func bind<T: Codable>(keyspace: String,_ onChange: (([T]?)->Void)?) -> BindingCollection<T>
+    @discardableResult func bind<T: Codable>(parameters:[param],_ onChange: (([T]?)->Void)?) -> BindingCollection<T>
+    @discardableResult func bind<T: Codable>(keyspace: String, parameters:[param],_ onChange: (([T]?)->Void)?) -> BindingCollection<T>
+    
+}
+
+internal protocol SwitchbladeBinding {
+    var id: UUID { get }
+    var key: KeyType? { get }
+    var keyspace: String? { get }
+    var count: Int { get }
+    func notify()
+}
