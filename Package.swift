bbc746d152c852de6a606c5908e8bd2e6795f836
Adrian Herridge <adrian@uncia.co.uk>
2021-10-27 13:56:58 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 let package = Package(
     name: "Switchblade",
-#if os(macOS) || os(Linux)
-    platforms: [
-        .macOS(.v10_15)
-    ],
-#endif
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
