d76ba4c778d80feb3b341c7e38706ba0d4835b73
Adrian Herridge <adrian@herridges.com>
2023-12-08 07:18:40 +0000

    updated dependency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["Switchblade"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .branch("main")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
